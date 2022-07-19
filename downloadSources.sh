a1a1f0c55e1972386c69ddcaa79bfa9006f0ca45
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-07-19 15:41:51 +0200

    add source downloader

---
+++ downloadSources.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source .env
+REPO=swiss-art-research-net/itten-data
+
+download () {
+  remotepath=$1
+  localpath=$2
+
+  echo -n "Downloading $remotepath: "
+  python3 scripts/getFileContentsFromGit.py $GITHUB_USERNAME $GITHUB_PERSONAL_ACCESS_TOKEN $REPO $remotepath $localpath
+}
+
+download "source/Update20220615_ver01.json" "data/source/cmi-export.json"
