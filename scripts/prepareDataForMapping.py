f868820f4594b053c64df496b7d1f3c72e81adfc
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-07-29 15:27:30 +0200

    add alignment data

--- scripts/prepareDataForMapping.py
+++ scripts/prepareDataForMapping.py
@@ -10,14 +10,18 @@ Usage:
 python prepareDataForMapping.py --sourceFolder=<sourceFolder> --oaiXMLFolder=<oaiXMLFolder> --outputFolder=<outputFolder>
 
 Parameters:
-    --sourceFolder       The folder containing the source JSON data
-    --oaiXMLFolder       The folder containing the XML data retrieved from e-manuscripta
-    --outputFolder       The folder to write the output XML files to
-    --vlidMapFile        The path to the file containing the mapping between VLIDs and DOIs (optional)
-    --onlyWithDoi        If set to true, only records that contain a DOI are output
+    --sourceFolder        The folder containing the source JSON data
+    --oaiXMLFolder        The folder containing the XML data retrieved from e-manuscripta
+    --outputFolder        The folder to write the output XML files to
+    --alignmentDataPrefix The prefix of the alignment data files. Defaults to "alignment-" (optional)
+    --vlidMapFile         The path to the file containing the mapping between VLIDs and DOIs (optional)
+    --onlyWithDoi         If set to true, only records that contain a DOI are output (optional)
 """
 
+import csv
+import json
 import sys
+import unicodedata
 from dicttoxml import dicttoxml
 from lxml import etree
 from os.path import join, isfile
@@ -29,6 +33,7 @@ def prepareData(options):
     sourceFolder = options['sourceFolder']
     oaiXMLFolder = options['oaiXMLFolder']
     outputFolder = options['outputFolder']
+    alignmentDataPrefix = options['alignmentDataPrefix']
     vlidMapFile = options['vlidMapFile']
     
     # Read records from input folder
@@ -42,6 +47,9 @@ def prepareData(options):
 
     # Retrieve OAI records for records that have VLIDs
     oaiXmlData = retrieveOaiXMLData(records=records, oaiXMLFolder=oaiXMLFolder, vlidMapFile=vlidMapFile)
+
+    # Add alignment data
+    records = addAlignmentData(records, sourceFolder=sourceFolder, alignmentDataPrefix=alignmentDataPrefix)
     
     # Convert to XML
     recordsXML = convertRecordsToXML(records)
@@ -51,6 +59,95 @@ def prepareData(options):
 
     # Write to files
     writeXMLRecordsToFiles(recordsXML, outputFolder)
+
+def addAlignmentData(records, *, sourceFolder, alignmentDataPrefix):
+    
+    def customHash(l):
+        def NFD(s):
+            return unicodedata.normalize('NFD', s)
+
+        return hash(NFD(json.dumps(l, ensure_ascii=False)))
+
+    def getValueByPath(node, path):
+        if len(path) == 1:
+            return node
+        return getValueByPath(record[path[0]], path[1:])
+
+    def readAlignmentFiles(fieldsToAlign, sourceFolder, alignmentDataPrefix):
+        alignmentData = {}
+        for key, path in fieldsToAlign.items():
+            filename = join(sourceFolder, alignmentDataPrefix + key + ".csv")
+            try:
+                content = []
+                with open(filename, 'r') as f:
+                    reader = csv.DictReader(f)
+                    for row in reader:
+                        content.append(row)
+                
+                lookup = {}
+                for i, row in enumerate(content):
+                    lookupHash = customHash(row['value'])
+                    lookup[lookupHash] = i
+                alignmentData[key] = {
+                    "path": path,
+                    "lookup": lookup,
+                    "content": content
+                }
+            except:
+                print("Could not read alignment file: " + filename)
+                sys.exit(1)
+        return alignmentData
+    
+    def setValueByPathAndKey(record, path, key, value, index=False):
+        """
+        Mutates the original record. A bit hacky and only supports up to 3 levels of nesting.
+
+        :param record: the record to mutate
+        :param path: the path to the node to mutate provided as a list of strings, e.g. ['Archivalienarten', 'Bezeichnung']
+        """
+        if len(path) == 1:
+            if (isinstance(record[path[0]], list)):
+                record[index] = value
+            else:
+                record[path[0]] = value
+        if len(path) == 2:
+            if isinstance(record[path[0]], list):
+                record[path[0]][index] = value
+            else:
+                record[path[0]][path[1]] = value
+        if len(path) == 3:
+            if isinstance(record[path[0]], list):
+                record[path[0]][index][path[1]] = value
+            else:
+                record[path[0]][path[1]][path[2]] = value
+       
+    fieldsToAlign = {
+        "archivalienarten": ["Archivalienarten", "Bezeichnung"]
+    }
+
+    alignmentData = readAlignmentFiles(fieldsToAlign, sourceFolder=sourceFolder, alignmentDataPrefix=alignmentDataPrefix)
+    
+    for field, data in alignmentData.items():
+        for record in records:
+            # Get record value by path
+            values = getValueByPath(record, data['path'])
+            key = data['path'][-1]
+            if isinstance(values, list):
+                for index, value in enumerate(values):
+                    if key in value:
+                        try:
+                            alignmentValues = data['content'][data['lookup'][customHash(value[key])]]
+                        except:
+                            print("Could not find alignment value for: " + str(value))
+                            sys.exit(1)
+                        for alignmentKey, alignmentValue in alignmentValues.items():
+                            if alignmentKey not in ['key', 'path', 'value']:
+                                value[alignmentKey] = alignmentValue
+                    setValueByPathAndKey(record, data['path'], key, value, index)
+
+
+    return records
+            
 
 def addOaiXMLData(records, oaiData):
     """
@@ -203,6 +300,9 @@ if __name__ == "__main__":
 
     if not 'vlidMapFile' in options:
         options['vlidMapFile'] = join(options['sourceFolder'], 'map_doi_vlid.csv')
+
+    if not 'alignmentDataPrefix' in options:
+        options['alignmentDataPrefix'] = 'alignment-'
     
     if 'limit' in options:
         options['limit'] = int(options['limit'])
@@ -212,5 +312,7 @@ if __name__ == "__main__":
             options['onlyWithDoi'] = True
         else:
             options['onlyWithDoi'] = False
+    else:
+        options['onlyWithDoi'] = False
 
     prepareData(options)
