38c5ba81804ee10511f1137c06f2d62bd6fcfb44
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-07-29 12:09:53 +0200

    prepare and merge data

--- scripts/prepareDataForMapping.py
+++ scripts/prepareDataForMapping.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 from lib.utils import readRecords, RetrieveVLIDfromDOI
 
-def performMerge(options):
+def prepareData(options):
     sourceFolder = options['sourceFolder']
     oaiXMLFolder = options['oaiXMLFolder']
     outputFolder = options['outputFolder']
@@ -15,47 +15,107 @@ def performMerge(options):
     # Read records from input folder
     records = readRecords(sourceFolder)
 
+    if 'onlyWithDoi' in options and options['onlyWithDoi'] == 'true':
+        records = [d for d in records if 'doi' in d]
+
+    if 'limit' in options:
+        records = records[:options['limit']]
+
+    # Retrieve OAI records for records that have VLIDs
+    oaiXmlData = retrieveOaiXMLData(records=records, oaiXMLFolder=oaiXMLFolder, vlidMapFile=vlidMapFile)
+    
+    # Convert to XML
+    recordsXML = convertRecordsToXML(records)
+
+    # Add data from OAI XML files
+    recordsXML = addOaiXMLData(recordsXML, oaiXmlData)
+
+    # Write to files
+    writeXMLRecordsToFiles(recordsXML, outputFolder)
+
+def addOaiXMLData(records, oaiData):
+
+    def removeUnneededTags(node):
+        pathsToRemove = [
+            ".//{http://www.loc.gov/METS/}metsHdr",
+            ".//{http://www.loc.gov/METS/}fileSec",
+            ".//{http://www.loc.gov/METS/}structMap",
+            ".//{http://www.loc.gov/METS/}structLink"
+        ]
+        nodesToRemove = [item for sublist in [node.findall(d) for d in pathsToRemove] for item in sublist]
+        
+        for unneeded in nodesToRemove:
+            print(unneeded.tag)
+            unneeded.getparent().remove(unneeded)
+            
+        return node
+
+    for record in records:
+        guid = record.find('guid').text
+        if guid in oaiData:
+            oaiNode = etree.SubElement(record, 'oai')
+            oaiNode.append(removeUnneededTags(oaiData[guid]).getroot())
+    return records
+
+def convertRecordsToXML(records):
+
+    def convertCmiJSONtoXML(record):
+
+        def cleanTag(tag):
+            cleanTag = replace_umlauts(tag.lower())
+            cleanTag = cleanTag.replace(' ', '_')
+            cleanTag = cleanTag.replace('/', '-')
+            return cleanTag
+
+        def replace_umlauts(text:str) -> str:
+            """replace special German umlauts (vowel mutations) from text. 
+            ä -> ae...
+            ü -> ue 
+            """
+            vowel_char_map = {ord('ä'):'ae', ord('ü'):'ue', ord('ö'):'oe', ord('ß'):'ss'}
+            return text.translate(vowel_char_map)
+
+        def convertToSafeKeys(dictionary):
+            newDict = {}
+            for key, value in dictionary.items():
+                if isinstance(value, dict):
+                    newDict[cleanTag(key)] = convertToSafeKeys(value)
+                elif isinstance(value, list):
+                    newDict[cleanTag(key)] = [convertToSafeKeys(v) for v in value]
+                else:
+                    newDict[cleanTag(key)] = value
+            return newDict
+
+        cleanedRecord = convertToSafeKeys(record)
+        
+        xml = dicttoxml(cleanedRecord, attr_type=False)
+        return etree.fromstring(xml)
+    
+    xmlRecords = []
+    for record in records:
+        xmlRecords.append(convertCmiJSONtoXML(record))
+    return xmlRecords
+
+def retrieveOaiXMLData(*, records, oaiXMLFolder, vlidMapFile):
+    oaiXmlData = {}
+
     # Retrieve VLIDs for records that have DOIs    
     vlidRetriever = RetrieveVLIDfromDOI(vlidMapFile=vlidMapFile)
+
     for record in tqdm([d for d in records if 'doi' in d]):
-        record['vlid'] = vlidRetriever.getVlidForDoi(record['doi'])
-
-    for record in [d for d in records if 'doi' in d]: # for now just use the ones with a doi
-        if record['vlid'] is not None:
-            filename = join(oaiXMLFolder, record['vlid'] + ".xml")
+        vlid = vlidRetriever.getVlidForDoi(record['doi'])
+        if vlid is not None:
+            filename = join(oaiXMLFolder, vlid + ".xml")
             if isfile(filename):
-                oaiData = etree.parse(filename)
-        root = convertCmiJSONtoXML(record)
-        outputFile = join(outputFolder, record['GUID'] + ".xml")
-        with open(outputFile, 'wb') as f:
-            f.write(root)
+                oaiXmlData[record['GUID']] = etree.parse(filename)
+    
+    return oaiXmlData
 
-
-def convertCmiJSONtoXML(record):
-
-    def cleanTag(tag):
-        cleanTag = replace_umlauts(tag.lower())
-        cleanTag = cleanTag.replace(' ', '_')
-        return cleanTag
-
-    def replace_umlauts(text:str) -> str:
-        """replace special German umlauts (vowel mutations) from text. 
-        ä -> ae...
-        ü -> ue 
-        """
-        vowel_char_map = {ord('ä'):'ae', ord('ü'):'ue', ord('ö'):'oe', ord('ß'):'ss'}
-        return text.translate(vowel_char_map)
-
-    cleanedRecord = {}
-    oldKeys = []
-
-    for key in record.keys():
-        oldKeys.append(key)
-    for key in oldKeys:
-        cleanedRecord[cleanTag(key)] = record[key]
-
-    return dicttoxml(cleanedRecord, attr_type=False)
-
+def writeXMLRecordsToFiles(records, outputFolder):
+    for i, record in enumerate(records):
+        filename = join(outputFolder, record.find('guid').text + ".xml")
+        with open(filename, 'wb') as f:
+            f.write(etree.tostring(record, pretty_print=True))
 
 if __name__ == "__main__":
     options = {}
@@ -82,4 +142,7 @@ if __name__ == "__main__":
     if not 'vlidMapFile' in options:
         options['vlidMapFile'] = join(options['sourceFolder'], 'map_doi_vlid.csv')
     
-    performMerge(options)
+    if 'limit' in options:
+        options['limit'] = int(options['limit'])
+
+    prepareData(options)
