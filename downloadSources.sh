214047d720490814f71cd61079405247610dd313
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-06-21 14:57:25 +0200

    include repo

--- downloadSources.sh
+++ downloadSources.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 source .env
-REPO=swiss-art-research-net/itten-data
 
 download () {
-  remotepath=$1
-  localpath=$2
+  repo=$1
+  remotepath=$2
+  localpath=$3
 
   echo -n "Downloading $remotepath: "
-  python3 scripts/getFileContentsFromGit.py $GITHUB_USERNAME $GITHUB_PERSONAL_ACCESS_TOKEN $REPO $remotepath $localpath
+  python3 scripts/getFileContentsFromGit.py $GITHUB_USERNAME $GITHUB_PERSONAL_ACCESS_TOKEN $repo $remotepath $localpath
 }
 
-download "source/source.json" "data/source/cmi-export.json"
-download "data/csv/archivalienarten.csv" "data/source/alignment-archivalienarten.csv"
-download "data/csv/register_id.csv" "data/source/alignment-register_id.csv"
-download "data/csv/sprachen.csv" "data/source/alignment-sprachen.csv"
-download "data/csv/verzeichnungsstufe.csv" "data/source/alignment-verzeichnungsstufe.csv"
-download "data/rdf/conceptLabels.trig" "data/ttl/additional/conceptLabels.trig"
-download "data/rdf/networkLabels.trig" "data/ttl/additional/networkLabels.trig"
+download "swiss-art-research-net/itten-data" "source/source.json" "data/source/cmi-export.json"
+download "swiss-art-research-net/itten-data" "data/csv/archivalienarten.csv" "data/source/alignment-archivalienarten.csv"
+download "swiss-art-research-net/itten-data" "data/csv/register_id.csv" "data/source/alignment-register_id.csv"
+download "swiss-art-research-net/itten-data" "data/csv/sprachen.csv" "data/source/alignment-sprachen.csv"
+download "swiss-art-research-net/itten-data" "data/csv/verzeichnungsstufe.csv" "data/source/alignment-verzeichnungsstufe.csv"
+download "swiss-art-research-net/itten-data" "data/rdf/conceptLabels.trig" "data/ttl/additional/conceptLabels.trig"
+download "swiss-art-research-net/itten-data" "data/rdf/networkLabels.trig" "data/ttl/additional/networkLabels.trig"
 
-download "external/heidelberg/2023-05-03_wvitten03.nq" "data/external/heidelberg.nq"
-download "external/sikart/sikart.ttl" "data/ttl/additional/sikart.ttl"
+download "swiss-art-research-net/itten-data" "external/sikart/sikart.ttl" "data/ttl/additional/sikart.ttl"
+download "swiss-art-research-net/itten-data" "external/heidelberg/2023-05-03_wvitten03.nq" "data/external/heidelberg.nq"
+download "swiss-art-research-net/sikart-data" "data/ttl/all_extracted.ttl" "data/external/sikart-extract.ttl"
