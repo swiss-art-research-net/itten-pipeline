fa99081d78729054414db3979c599e82eca6ccb7
Florian Kräutli <florian.kraeutli@uzh.ch>
2023-06-30 15:32:09 +0200

    add agrelon

--- downloadSources.sh
+++ downloadSources.sh
@@ -17,6 +17,7 @@ download "swiss-art-research-net/itten-data" "data/csv/sprachen.csv" "data/sourc
 download "swiss-art-research-net/itten-data" "data/csv/verzeichnungsstufe.csv" "data/source/alignment-verzeichnungsstufe.csv"
 download "swiss-art-research-net/itten-data" "data/rdf/conceptLabels.trig" "data/ttl/additional/conceptLabels.trig"
 download "swiss-art-research-net/itten-data" "data/rdf/networkLabels.trig" "data/ttl/additional/networkLabels.trig"
+download "swiss-art-research-net/itten-data" "external/gnd/agrelon_20191015.ttl" "data/ttl/additional/agrelon.ttl"
 
 download "swiss-art-research-net/itten-data" "external/sikart/sikart.ttl" "data/ttl/additional/sikart.ttl"
 download "swiss-art-research-net/itten-data" "external/heidelberg/2023-05-03_wvitten03.nq" "data/external/heidelberg.nq"
